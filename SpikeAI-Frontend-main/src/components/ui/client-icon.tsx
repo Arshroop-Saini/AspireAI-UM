32a15e67a275ed6f438c4f97821e77c1d6c61c2a
Arshroop Singh Saini <77531443+Arshroop-Saini@users.noreply.github.com>
2025-02-23 18:45:01 -0500

    Delete SpikeAI-Frontend-main directory

--- SpikeAI-Frontend-main/src/components/ui/client-icon.tsx
+++
@@ -1,9 +0,0 @@
-import { LucideIcon } from "lucide-react";
-
-interface IconProps {
-    icon: LucideIcon;
-}
-
-export function ClientIcon({ icon: Icon }: IconProps) {
-    return <Icon className="h-4 w-4" />;
-} 
