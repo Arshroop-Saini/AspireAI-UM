625df7ea52b53f0263338e9458823932e41a65c2
Arshroop Singh Saini <77531443+Arshroop-Saini@users.noreply.github.com>
2025-02-23 18:44:22 -0500

    Delete SpikeAI-Backend-main directory

--- SpikeAI-Backend-main/agent_crews/essay_brainstorm_crews/tools/__init__.py
+++
@@ -1,9 +0,0 @@
-from .profile_tool import StudentProfileTool
-from .ideas_history_tool import IdeasHistoryTool
-from .search_tool import CollegeSearchTool
-
-__all__ = [
-    'StudentProfileTool',
-    'IdeasHistoryTool',
-    'CollegeSearchTool'
-] 
--- SpikeAI-Backend-main/config/serper_config.py
+++
@@ -1,9 +0,0 @@
-from os import environ as env
-
-SERPER_API_KEY = env.get('SERPER_API_KEY')
-
-def get_serper_api_key() -> str:
-    """Get Serper API key from environment"""
-    if not SERPER_API_KEY:
-        raise ValueError("SERPER_API_KEY environment variable is not set")
-    return SERPER_API_KEY 
